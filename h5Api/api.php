09fca7f6a5a1727c4a43c4842b1eff69309eb3c6
Nicklasx <NPMa.skp@edu.mercantec.dk>
2020-10-26 12:48:04 +0100

    Tilføjelser til api
    Gjort så man kan få en liste med alle brugere oprette ny bruger og slette brugere

--- h5Api/api.php
+++ h5Api/api.php
@@ -14,16 +14,72 @@
             $this->route = new Route($this->db);
         }
 
-        function login()
-        {
+        function login() {
             $this->dbConn();
             $data = file_get_contents('php://input', true);
             $jsonData = json_decode($data);
             $username = $jsonData->username;
             $password = $jsonData->password;
-            if ($login = $this->route->verifyLogin($username, $password)) {
+            if($login = $this->route->verifyLogin($username, $password)) {
                     $output = array("status" => $login);
                     echo json_encode($output);
+            } else {
+                // set responce code - 404 Not found
+                http_response_code(404);
+
+                // tell the user no users found
+                echo json_encode(
+                    array("message" => "failed to login.")
+                );
+            }
+        }
+
+        function selectUsers() {
+            $this->dbConn();
+            // $data = file_get_contents('php://input', true);
+            // $jsonData =  json_decode($data);
+            // $username = $jsonData->username;
+            if($select = $this->route->selectUsers()) {
+                echo json_encode($select);
+            } else {
+                // set responce code - 404 Not found
+                http_response_code(404);
+
+                // tell the user no users found
+                echo json_encode(
+                    array("message" => "failed to login.")
+                );
+            }
+        }
+
+        function createUser() {
+            $this->dbConn();
+            $data = file_get_contents('php://input', true);
+            $jsonData =  json_decode($data);
+            $username = $jsonData->username;
+            $password = $jsonData->password;
+            if($create = $this->route->createUser($username, $password)) {
+                $output = array("status" => $create);
+                echo json_encode($output);
+            } else {
+                // set responce code - 404 Not found
+                http_response_code(404);
+
+                // tell the user no users found
+                echo json_encode(
+                    array("message" => "failed to login.")
+                );
+            }
+        }
+
+        function deleteUser() {
+            $this->dbConn();
+            $data = file_get_contents('php://input', true);
+            $jsonData =  json_decode($data);
+            $username = $jsonData->username;
+            if($delete = $this->route->deleteUser($username)) {
+                $output = array("status" => $delete);
+                echo json_encode($output);
             } else {
                 // set responce code - 404 Not found
                 http_response_code(404);
@@ -46,7 +102,15 @@ switch ($func) {
     case 'login':
         $funcList->login();
         break;
-
+        case 'selectUsers':
+            $funcList->selectUsers();
+            break;
+    case 'createUser':
+        $funcList->createUser();
+        break;
+    case 'deleteUser':
+        $funcList->deleteUser();
+        break;
     default:
         # code...
         break;
--- h5Api/route.php
+++ h5Api/route.php
@@ -1,9 +1,11 @@
 <?php
-    // ini_set('display_errors', 1);
+    ini_set('display_errors', 1);
     class Route {
         // database connection
         private $conn;
 
+        public $username;
+        public $password;
         // constructor with $db as database connection
         public function __construct($db)
         {
@@ -39,6 +41,83 @@
                 }
                 //set values to obiest properties
 
+            }
+        }
+
+        function selectUsers() {
+
+            // query
+            $query =
+                "SELECT *
+                FROM users";
+        
+            // prepare query
+            $stmt = $this->conn->prepare($query);
+        
+            // execute query
+            $stmt->execute();
+
+            // get retrived row
+            $userArr = array();
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                array_push($userArr, $row['username']);
+            }
+            print_r($userArr);
+            $jsonOut = array("usernames" => $userArr);
+            return $jsonOut;
+        
+            // set valuse to objest properties
+            // $usernames = $row['username'];
+        
+            // return $test;
+        }
+
+        function createUser(string $username, string $password) {
+            //query
+            $query =
+            "INSERT INTO users
+            SET username = :username,
+            password = :password";
+
+            // prepare query
+            $stmt = $this->conn->prepare($query);
+
+            // sanitizee (remove HTML tags)
+            $this->username = htmlspecialchars(strip_tags($username));
+            $this->password = htmlspecialchars(strip_tags($password));
+
+            // bind values
+            $stmt->bindParam(":username", $this->username, PDO::PARAM_STR);
+            $stmt->bindParam(":password", $this->password, PDO::PARAM_STR);
+
+            // execute query
+            if ($stmt->execute()) {
+                return "user created";
+            } else {
+                return "user not created";
+            }
+        }
+
+        function deleteUser(string $username) {
+
+            // query
+            $query =
+                "DELETE FROM users
+                WHERE username = :username";
+        
+            // prepare query statement
+            $stmt = $this->conn->prepare($query);
+        
+            // sanitize (remove HTML tags)
+            $this->username = htmlspecialchars(strip_tags($username));
+        
+            $stmt->bindParam(":username", $this->username, PDO::PARAM_STR);
+        
+            // execute the query
+            if ($stmt->execute()) {
+                return "user deleted";
+            } else {
+                return "user not deleted";
             }
         }
     }
